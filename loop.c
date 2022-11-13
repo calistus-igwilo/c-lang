5e26fb26815c6b832106f8ffd45be4eb70637b8a
calistus-igwilo <ebunilo@yahoo.com>
2022-11-13 23:06:27 +0100

    loops

---
+++ loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 0;
+    for(i=0; i<3; i++) {
+        printf("C is fun!\n");
+    }
+    printf("\n");
+    
+    for(i=0; i<5; i++) {
+        printf("We can do everything with it!\n");
+    }
+    
+    return 0;
+
+}
