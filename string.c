3814e591e994298ec8471b38b1cd7389df84e0c8
calistus-igwilo <ebunilo@yahoo.com>
2022-11-13 17:05:44 +0100

    array

--- string.c
+++ string.c
@@ -3,8 +3,19 @@
 
 int main(){
     char name[50] = "Ebunilo";
+    int prices[5];
+
+    printf("%s\n", name);
 
     /* name = "Calistus"; */
     strcpy(name, "Calistus");
-    printf("%s", name);
+    printf("%s\n", name);
+
+    for (int i=0; i<5; i++){
+        prices[i] = i + 1;
+    }
+
+    printf("%d\n", *prices); /* 1 */
+    printf("%d\n", *(prices + 1)); /* 2 */
+    printf("%d\n", *(prices + 2)); /* 3 */
 }
