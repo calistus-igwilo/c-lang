efb7ba67846da0b57edd4f87202fb8b95cbd1c41
calistus-igwilo <ebunilo@yahoo.com>
2022-11-26 21:33:02 +0100

    Pass struct to functions

---
+++ structTofunction2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct date {
+        int year;
+        int month;
+        int day;
+    };
+
+void readDate(struct date *);
+void printDate(struct date);
+
+int main(void) {
+	struct date today;
+	readDate(&today);
+	printDate(today);
+	return 0;
+}
+
+void readDate(struct date *today) {
+    scanf("%d %d %d", &(*today).year, &(*today).month, &(*today).day);
+}
+
+void printDate(struct date today) {
+    printf("%02d/%02d/%04d", today.month, today.day, today.year);
+}
